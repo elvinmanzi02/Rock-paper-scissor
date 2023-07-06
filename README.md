b3ce7132c96056e4dd4c9580c7c02a9ef79a5cff
elvinmanzi02 <elvinmanzi@gmail.com>
2023-07-06 09:16:29 +0200

    modification of getComputerChoice() and roundSelector() called determineWinner and new function created called game()

--- README.md
+++ README.md
@@ -1,57 +1,39 @@
-// Game is going to play against computer
-// 1 getComputerChoice() function return roundomly 'Rock', 'paper or scissor
-// 2 function(parameter1, parameter2) with player selection and computer selection and will return "You lose! paper beats Rock"
-// 2 game function
+// 1 create function getComputerChoice
+// 2 creat function determineWinner
 
+// function one
 
-function getComputerChoice(min = 1, max = 3) {
-    const result = Math.floor(Math.random()*(max-min + 1)) + min;
-    if (result === 1) {
-        return "Rock";
-    } else if ( result === 2) {
-        return "Paper";
+function getComputerChoice() {
+    const choices = ["Rock","Paper","Scissor"];
+    randomIndex = Math.floor(Math.random()*choices.length);
+    return choices[randomIndex];
+}
+
+// function two
+
+function determineWinner(playerChoice, computerChoice) {
+
+    if (playerChoice === computerChoice) {
+        return "It's tie";
+    }else if (
+        (playerChoice === "Rock" && computerChoice === "Scissor") ||
+        (playerChoice === "Paper" && computerChoice === "Rock") ||
+        (playerChoice === "Scissor" && computerChoice === "Paper")   
+    ) {
+        return `Congratulations you win! ${playerChoice} beats ${computerChoice}`;
     }else {
-        return "Scissor";
-    }
-}
-const computerSelector = getComputerChoice();
-
-function roundSeletor(playerSelector, computerSelector) {
-    if (playerSelector === "rock") {
-        if(computerSelector === "Scissor") {
-            return "You win! Rock beats scissor";
-        }else if (computerSelector === "Rock") {
-            return "Draw!";
-        }else {
-            return "You lose! Paper beats rock";
-        }
-    } else if(playerSelector === "scissor") {
-        if(computerSelector === "Scissor") {
-            return "Draw!";
-        }else if (computerSelector === "Rock") {
-            return "You lose! Rock beats scissor";
-        }else {
-            return "You win! Scissor beats paper";
-        }
-    }
-
-    else {
-        if(computerSelector === "Scissor") {
-            return "You lose!, Scissor beats paper";
-        }else if (computerSelector === "Rock") {
-            return "You win! Paper beats rock";
-        }else {
-            return "Draw!";
-        }
+        return `Ooh sorry try again! ${computerChoice} beats ${playerChoice}`;
     }
 }
 
-const playerChoice = "Rock";
-const playerSelector = playerChoice.toLowerCase();
-
-function game() {
-    for(let i = 1; i <= 5; i++) {
-        
-        roundSeletor(playerSelector, computerSelector);
-    }
+function playGame(playerChoice) {
+    const computerChoice = getComputerChoice();
+    const result = determineWinner(playerChoice, computerChoice);
+    console.log(`playerChoice ${playerChoice}`);
+    console.log(`computerChoice ${computerChoice}`)
+    return result;
 }
+ 
+const playerChoice = "Scissor";
+
+console.log(playGame(playerChoice));
--- main.js
+++ main.js
@@ -1,54 +1,41 @@
-// Game is going to play against computer
-// 1 getComputerChoice() function return roundomly 'Rock', 'paper or scissor
-// 2 function(parameter1, parameter2) with player selection and computer selection and will return "You lose! paper beats Rock"
-// 2 game function
+// 1 create function getComputerChoice
+// 2 creat function determineWinner
+// 3 create function of 
 
-// function one that let computer selects 
+// function one
 
-function getComputerChoice(min = 1, max = 3) {
-    const result = Math.floor(Math.random()*(max-min + 1)) + min;
-    if (result === 1) {
-        return "Rock";
-    } else if ( result === 2) {
-        return "Paper";
+function getComputerChoice() {
+    const choices = ["Rock","Paper","Scissor"];
+    randomIndex = Math.floor(Math.random()*choices.length);
+    return choices[randomIndex];
+}
+
+// function two
+
+
+function determineWinner(playerChoice, computerChoice) {
+
+    if (playerChoice === computerChoice) {
+        return "It's tie";
+    }else if (
+        (playerChoice === "Rock" && computerChoice === "Scissor") ||
+        (playerChoice === "Paper" && computerChoice === "Rock") ||
+        (playerChoice === "Scissor" && computerChoice === "Paper")   
+    ) {
+        return `Congratulations you win! ${playerChoice} beats ${computerChoice}`;
     }else {
-        return "Scissor";
-    }
-}
-const computerSelector = getComputerChoice();
-
-// function two that that decide who wins
-
-function roundSeletor(playerSelector, computerSelector) {
-    if (playerSelector === "rock") {
-        if(computerSelector === "Scissor") {
-            return "You win! Rock beats scissor";
-        }else if (computerSelector === "Rock") {
-            return "Draw!";
-        }else {
-            return "You lose! Paper beats rock";
-        }
-    } else if(playerSelector === "scissor") {
-        if(computerSelector === "Scissor") {
-            return "Draw!";
-        }else if (computerSelector === "Rock") {
-            return "You lose! Rock beats scissor";
-        }else {
-            return "You win! Scissor beats paper";
-        }
-    }
-
-    else {
-        if(computerSelector === "Scissor") {
-            return "You lose!, Scissor beats paper";
-        }else if (computerSelector === "Rock") {
-            return "You win! Paper beats rock";
-        }else {
-            return "Draw!";
-        }
+        return `Ooh sorry try again! ${computerChoice} beats ${playerChoice}`;
     }
 }
 
-const playerChoice = "Rock";
-const playerSelector = playerChoice.toLowerCase();
-console.log (roundSeletor(playerSelector, computerSelector));
+function game(playerChoice) {
+    const computerChoice = getComputerChoice();
+    const result = determineWinner(playerChoice, computerChoice);
+    console.log(`playerChoice ${playerChoice}`);
+    console.log(`computerChoice ${computerChoice}`)
+    return result;
+}
+ 
+const playerChoice = "Paper";
+
+console.log(game(playerChoice));
